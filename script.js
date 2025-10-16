3beb5f4d1193efcf4547953e65b3145d61489238
Hairy Potter <HideInBetweenLeg@users.noreply.github.com>
2025-10-16 20:35:51 +0800

    bar with filter added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var vg_evmap = "ev_map.json";
 vegaEmbed("#vis", vg_evmap, {actions: false}).then(function(res){}).catch(console.error);
 
-var vg_gdp_lollipop = "gdp_lollipop.json";
+var vg_gdp_lollipop = "gdp_scatter.json";
 vegaEmbed("#gdp_chart", vg_gdp_lollipop, { actions: false })
   .then(function (result) {})
   .catch(console.error);
+
